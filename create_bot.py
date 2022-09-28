edb26102890e33dd084f19034358ed42d4f695e5
Aresmp3 <daniilpurisov390@gmail.com>
2022-09-28 20:59:29 +0300

    dotenv

--- create_bot.py
+++ create_bot.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import os
+from dotenv import load_dotenv
 
 
 AVAILABLE_GAMES = {'anagrams': 'Анаграммы', 'croco': 'Крокодил', 'sticks': 'Палочки', 'months': 'Месяца'}
@@ -10,8 +11,8 @@ session = {}
 with open('10000-russian-words.txt', 'r', encoding='utf-8') as f:
     words = f.read().splitlines()
 
+
+load_dotenv()
+
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot, storage=MemoryStorage())
-
-
-
